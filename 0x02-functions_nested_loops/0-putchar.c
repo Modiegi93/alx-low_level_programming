1287a61f9c2b7e5012f6a300eb72bfde43fa3d55
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-13 03:59:46 -0700

    header_file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Returns: Always 0
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
