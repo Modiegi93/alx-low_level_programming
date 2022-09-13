c3196ac1046ca896098705a1d06b78d27a66fd1c
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-13 07:16:19 -0700

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int tenx;
 
-	for (tenx = 0; tenx <= 10; tenx++)
+	for (tenx = 0; tenx < 10; tenx++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
