593e919e236273915cf7d50238a7138399661076
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-13 13:39:42 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - write a function that adds two integers
+ * @n: an integer
+ * @n2: an integer
+ *
+ * Description: function to add two numbers
+ * Return: the addition of two integers
+ */
+int add(int n, int n2)
+{
+	int m;
+
+	m = n + n2;
+
+	return (m);
+}
+
