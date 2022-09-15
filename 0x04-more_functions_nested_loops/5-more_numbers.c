a16f8be85f5355bd5da63f902e6bab4409cddef5
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-15 10:58:01 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; ++a)
+	{
+		for (b = 0; b < 15; ++b)
+		{
+			if (a > 9)
+				_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+		}
+		_putchar('\n');
+	}
+}
+
