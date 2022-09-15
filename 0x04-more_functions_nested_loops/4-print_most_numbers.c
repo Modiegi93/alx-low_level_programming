45a87aa9474249c5d909a3786e4c3cda681aca3c
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-15 10:37:38 -0700

    fix code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,17 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * except 2 and 4
  *
- * Return: 0-9 excluding 2 and 4
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if ((i != 2) && (i != 4))
+		if ((i != '2') && (i != '4'))
 			_putchar(i);
 	}
 	_putchar('\n');
