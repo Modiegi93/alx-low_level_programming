218389781e7d18da41daa1fb88e86b9917a6fb8c
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-19 10:07:36 -0700

    fix errors

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,9 +4,9 @@
 /**
  * print_array - Write a function that prints n elements of
  * an array of integers, followed by a new line.
- * @n: integer
- * @a: an array
- * Return: array of integers
+ * @n: the number of array elements
+ * @a: an array of integers
+ * Return: void
  */
 void print_array(int *a, int n)
 {
