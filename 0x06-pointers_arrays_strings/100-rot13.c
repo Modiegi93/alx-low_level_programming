63f16504227146b658cb54ca0167caa05aeadc08
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-22 05:39:14 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13.
+ * @s: string to encode
+ *
+ * Return: pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	char *c;
+	char shift;
+
+	for (c = s; *c; ++c)
+	{
+		shift = 'A' + (c && 32);
+		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+			*c = (*c - shift + 13) % 26 + shift;
+	}
+}
