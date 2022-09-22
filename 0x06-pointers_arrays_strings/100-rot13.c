6222a3d880c3435f28690b80a659e4a531529d01
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-22 06:04:08 -0700

    fix code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 
 	for (c = s; *c; ++c)
 	{
-		shift = 'A' + (c && 32);
+		shift = 'A' + (*c & 32);
 		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
 			*c = (*c - shift + 13) % 26 + shift;
 	}
