1908cba8eca1db32133e02d7e1553a4717bdf1e5
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-22 11:29:06 -0700

    fix errors

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,8 @@
  * *rot13 - encodes a string using rot13.
  * @s: string to encode
  *
+ * Description: each letter is replaced with the 13th letter after it
+ *
  * Return: pointer to the encoded string
  */
 char *rot13(char *s)
