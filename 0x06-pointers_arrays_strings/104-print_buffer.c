2329cf42ec845d0c6dd461f4e12ec0c3c184526c
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-23 02:58:14 -0700

    fix code

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -24,25 +24,27 @@ void print_buffer(char *b, int size)
 	int b_pos;
 	int l_pos;
 
-	for (l_pos = 0; l_pos < 10; ++l_pos)
+	for (b_pos = 0; b_pos < size; b_pos += 10)
 	{
+		printf("%08x: ", b_pos);
+		for (l_pos = 0; l_pos < 10; ++l_pos)
+		{
+			if (b_pos + l_pos < size)
+				printf("%02x", b[b_pos + l_pos]);
+			else
+				printf(" ");
+			if (l_pos % 2)
+				putchar(' ');
+		}
+		for (l_pos = 0; l_pos < 10 && b_pos + l_pos < size; ++l_pos)
+		{
+			if (b[b_pos + l_pos] < 32 || b[b_pos + l_pos] > 126)
+				putchar('.');
+			else
+				putchar(b[b_pos + l_pos]);
+		}
 		if (b_pos + l_pos < size)
-			printf("%02x", b[b_pos + l_pos]);
-		else
-			printf(" ");
-		if (l_pos % 2)
-			putchar(' ');
+			putchar('\n');
 	}
-	for (l_pos = 0; l_pos < 10 && b_pos + l_pos < size; ++l_pos)
-	{
-		if (b[b_pos + l_pos] < 32 || b[b_pos + l_pos] > 126)
-			putchar('.');
-		else
-			putchar(b[b_pos + l_pos]);
-	}
-	if (b_pos + l_pos < size)
 		putchar('\n');
-	{
-		putchar('\n');
-	}
 }
