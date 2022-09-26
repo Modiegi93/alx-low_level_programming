50f15f3d30e665d76dd9ddc81063ebde045cdbad
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-26 08:33:20 -0700

    fix code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,7 +10,7 @@ void print_chessboard(char (*a)[8])
 {
 	int row, col;
 
-	for (row = 0; row < 0; ++row)
+	for (row = 0; row < 8; ++row)
 	{
 		for (col = 0; col < 8; ++col)
 			_putchar(a[row][col]);
