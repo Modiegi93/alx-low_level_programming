df3cda95be813bb0795088c3584499047f42c302
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-09-28 11:06:02 -0700

    fix errors

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,27 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calculate the square root of a number
+ * __sqrt_recursion - helper for _sqrt_recursion
  * @n: the number to calculate the square root of
  * @m: the divisor to test
  *
  * Return: the square root of n
  */
-int _sqrt_recursion(int n, int m)
+int __sqrt_recursion(int n, int m)
 {
 	if (m * m < n)
-		return (_sqrt_recursion(n, m + 1));
+		return (__sqrt_recursion(n, m + 1));
 	return (m * m == n ? m : -1);
+}
+
+/**
+ * _sqrt_recursion - compute the square root of a number
+ * @n: the number to compute the square root of
+ *
+ * Return: the natural square root of n,
+ * otherwise 0 if n has no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (__sqrt_recursion(n, 0));
 }
