2a733999b545b324a3561f5d86b57596a371f7b4
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-06 09:18:58 -0700

    initial commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,6 +15,13 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (!array)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = min++;
 
