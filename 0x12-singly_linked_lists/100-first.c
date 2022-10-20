ac73e5a796fa389714af331149222da10a8395b1
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-20 04:51:43 -0700

    fixed code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,14 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 
-void __attribute__((constructor)) primary(void);
-
 /**
- * primary - prints sentence before main function
+ * print_sen - prints sentence before main function
  *
  * Return: void
  */
-void primary(void)
+void __attribute__((constructor)) print_sen()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house");
 	printf("upon my back!\n");
