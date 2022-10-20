745a13e4958ccdb5e16f768ddf7c583345917e6f
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-20 04:31:01 -0700

    fix code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 
-void print_main(void);
+void __attribute__((constructor)) print_main(void);
 
 /**
  * print_main - prints before main function is executed
@@ -11,6 +11,6 @@ void print_main(void);
  */
 void print_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house");
+	printf("upon my back!\n");
 }
