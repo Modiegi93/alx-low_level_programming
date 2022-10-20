307a8925e77969626ca1bfea9f37aec8c4a9231e
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-20 06:06:53 -0700

    fixed code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-msg: DB 'Hello Holberton', 14
-msgSize EQU $ - msg
-
-global start
-
-section .text
-
-start:
-mov rax, 4		; function 4
-mov rbx, 1		; stdout
-mov rcx, msg		; msg
-mov rdx, msgSize	; size
-mov rax, 1		; function 1
-mov rbx, 0		; code
-ret
-
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
+	
+	section .text
+	extern printf
+	global main
+	
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
