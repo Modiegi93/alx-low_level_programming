8f46489ee70b9d0b63ac3743fe0bc7ad820de9e4
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-20 04:23:00 -0700

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+
+void print_main(void);
+
+/**
+ * print_main - prints before main function is executed
+ *
+ * Return: void
+ */
+void print_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
