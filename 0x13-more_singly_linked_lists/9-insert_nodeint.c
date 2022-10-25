e2e70a134683dd8407357d7f111112f710eafddb
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-10-25 01:42:38 -0700

    debugged code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,39 +11,40 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	listint_t *temp;
-	unsigned int i;
+	listint_t *x_node = *head;
+	unsigned int index;
+	unsigned int cont = 0;
 
+	/* create node */
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	temp = *head;
-	i = 0;
-	if (*head == NULL && idx > 0)
-	{
 
-		free(new_node);
-		return (NULL);
-	}
+	/* border case for insert at the beginning */
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
+		return (*head);
+	}
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (x_node && cont != index)
+	{
+		cont++;
+		x_node = x_node->next;
+	}
+
+	/* general case */
+	if (cont == index && x_node)
+	{
+		new_node->next = x_node->next;
+		x_node->next = new_node;
 		return (new_node);
 	}
-	while (i < idx - 1)
-	{
-		temp = temp->next;
-		if (temp == NULL && idx - i > 0)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		i++;
-	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+
+	free(new_node);
+	return (NULL);
 }
