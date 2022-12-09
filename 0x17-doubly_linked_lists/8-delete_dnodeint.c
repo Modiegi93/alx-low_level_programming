cf65555831a0a45e7dcd1493b43f89a5ad3d1ba0
Mathapelo <modiegi.mphahlele93@gmail.com>
2022-12-09 02:55:26 -0800

    fixed errors

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,14 +26,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = tmp->next;
 		if (*head != NULL)
-			(*head)->next = NULL;
+			(*head)->prev = NULL;
 	}
 
 	else
 	{
 		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
-			tmp->next->prev = tmp->next;
+			tmp->next->prev = tmp->prev;
 	}
 
 	free(tmp);
