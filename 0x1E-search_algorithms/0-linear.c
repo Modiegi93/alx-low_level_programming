023eb0506ce80277f1629a4e08ded3d167b627b8
Mathapelo Mphahlele <modiegi.mphahlele93@gmail.com>
2023-04-13 02:21:48 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,6 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
@@ -25,3 +24,4 @@ int linear_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
+
